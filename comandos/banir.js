34f6faab6e8b8634b150d15d9bfe8511923843b4
marshalito <40542263+marshalito@users.noreply.github.com>
2018-07-09 19:29:17 -0300

    Update and rename ban.js to banir.js

--- comandos/banir.js
+++ comandos/banir.js
@@ -4,13 +4,13 @@ module.exports.run = (bot, message, args) => {
   if (!message.guild.member(message.author).hasPermission("BAN_MEMBERS")) return null;
   if (!message.guild.member(bot.user).hasPermission("BAN_MEMBERS")) return message.reply("Eu não tenho a permissão para banir players!");
   if(!args[0]){
-      message.reply("**use:** !ban <player> <motivo>.");
+      message.reply("**use:** !banir <@membro> [motivo].");
       return;
   }
   let user = message.mentions.users.first();
   let reason = message.content.split(" ").slice(2).join(" ");
   if (message.mentions.users.size < 1) return message.reply ("você deve mencionar algum usuário!");
-  if (!message.guild.member(user).bannable) return message.reply("não posso kickar alguém que tenha um cargo mais acima do que o meu!");
+  if (!message.guild.member(user).bannable) return message.reply("não posso banir alguém que tenha um cargo mais acima do que o meu!");
   if (message.author.id === user.id) return message.reply("você não pode se banir.")
   message.guild.member(user).ban();
   message.reply("usuário **banido** com **sucesso**.").then(a=>a.delete(1500));
