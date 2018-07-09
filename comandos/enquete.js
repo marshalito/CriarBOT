17694127dbe029cd2a3f003f1a88fd185e230621
marshalito <40542263+marshalito@users.noreply.github.com>
2018-07-09 18:40:06 -0300

    Update enquete.js

--- comandos/enquete.js
+++ comandos/enquete.js
@@ -1,25 +1,22 @@
-const Discord = require('discord.js')
-module.exports.run = async (client, message, args) => {
-    if (!message.guild.member(message.author).hasPermission("MANAGE_MESSAGES")) return
-    if (!message.guild.member(client.user).hasPermission("ADMINISTRATOR")) return message.reply(":x: Eu não tenho a permissão `ADMINISTATOR`, estão descarte este comando, ou ative!!!!");
-    message.delete().catch(O_o=>{});
-    if (!args[0]) {
-        message.reply(":x: **Use:** !enquete (msg)");
-        return;
-    };
-    var args1 = message.content.split(" ").slice(1).join(" ");
-    if (!args1) return message.reply(":x: Digite !anuncio para saber mais");
+exports.run = async(client, message, args) => {
+    if(!message.guild.member(message.author).hasPermission("ADMINISTRADOR")) return;
+    if(!message.guild.member(client.user).hasPermission("ADMINISTRATOR")) return;
+    let la = message.content.split(" ").slice(1).join(" ");
+    if(!la) return message.channel.send("Insira o conteúdo da enquete para que ela possa ser feita!");
+    message.delete(O_o=>{});
+    const Discord = require('discord.js');
     message.channel.send('@everyone').then(a=>a.delete(1));
-    var role = message.guild.roles.find('name', 'Dream'); //Altera esse Dream caso altere o nome :D
-    const embed1 = new Discord.RichEmbed()
-  .setAuthor('Enquete...')
-  .setDescription(`${args1}`)
-  .setColor(role.color)
-  .setTimestamp()
-  .setFooter(`Anúncio por: ${message.author.username}`, message.author.avatarURL)
-  embed1.react("👍");
-  message.channel.send({embed: embed1}); 
+    var embed = new Discord.RichEmbed()
+        .setAuthor("📋 ENQUETE!")
+        .setDescription(`\n
+${la}`)
+        .setColor(role.color)
+        .setTimestamp();
+        .setFooter(`Enquete por: ${message.author.username}`, message.author.avatarURL)
+    message.channel.send({embed: embed}).then(msg=> {
+        msg.react('👎');
+        msg.react('👍');
+    });
 }
-module.exports.help = {
-    name: "enquete"
-    }
+
+exports.help = {name: "enquete"}
