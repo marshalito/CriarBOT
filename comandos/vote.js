94ab71084f28c218fefbc6b81a81ebc7b28075c9
marshalito <40542263+marshalito@users.noreply.github.com>
2018-07-09 18:53:35 -0300

    Update vote.js

--- comandos/vote.js
+++ comandos/vote.js
@@ -2,18 +2,18 @@ exports.run = async(client, message, args) => {
     if(!message.guild.member(message.author).hasPermission("ADMINISTRADOR")) return;
     if(!message.guild.member(client.user).hasPermission("ADMINISTRATOR")) return;
     let la = message.content.split(" ").slice(1).join(" ");
-    if(!la) return message.channel.send(":x:**|**Me dê o que você quer colocar em votação!");
+    if(!la) return message.channel.send("Insira detalhes de como será a enquete.");
     message.delete(O_o=>{});
     const Discord = require('discord.js');
     var embed1 = new Discord.RichEmbed()
-        .setAuthor("VOTAÇÃO!")
+        .setAuthor("ENQUETE!")
         .setDescription(`\n
 ${la}`)
         .setColor('RANDOM')
         .setTimestamp();
     message.channel.send({embed: embed1}).then(msg=> {
-        msg.react('👎');
         msg.react('👍');
+        msg.react('👎');
     });
 }
 module.exports.help = {
