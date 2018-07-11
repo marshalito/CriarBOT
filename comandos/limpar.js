012ea9cc115a11725345e59bd255cc3c3a8e38ba
marshalito <40542263+marshalito@users.noreply.github.com>
2018-07-11 15:40:35 -0300

    Update limpar.js

--- comandos/limpar.js
+++ comandos/limpar.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
   if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Permissões insuficientes.");
-  if(!args[0]) return message.channel.send("Especifique o número de mensagens que devem ser limpadas.");
+  if(!args[0]) return message.channel.send("Especifique o número de mensagens que devem ser limpadas, **entre 1 e 9**.");
   message.channel.bulkDelete(args[0]).then(() => {
   message.channel.send(`Foram removidas ${args[0]} mensagens.`).then(msg => msg.delete(2000));
 });
