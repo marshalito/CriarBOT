985f383470ff949df6324347e4a7c105e4199fca
marshalito <40542263+marshalito@users.noreply.github.com>
2018-08-06 16:14:48 -0300

    Update banir.js

--- comandos/banir.js
+++ comandos/banir.js
@@ -2,17 +2,8 @@ const Discord = require ("discord.js");
 module.exports.run = (bot, message, args) => {
   message.delete().catch(o_O=>{});
   if (!message.guild.member(message.author).hasPermission("BAN_MEMBERS")) return null;
-  if (!message.guild.member(bot.user).hasPermission("BAN_MEMBERS")) return message.reply("Eu não tenho a permissão para banir players!");
-  if (!args[0]){
-      const msg = new Discord.RichEmbed()
-      .setTitle('Comando: !banir')
-        .setDescription(`Como usar:\n
-**Descrição:** Banir membros.
-**Sintaxe:** !banir @membro <motivo>.
-**Dica:** Insira a prova entre [].`)
-       .setColor('#dcf442');
-       message.channel.send({ embed : msg });
-       return;
+  if (!message.guild.member(bot.user).hasPermission("BAN_MEMBERS")) return message.reply("permissões insuficientes");
+
   }    
   let user = message.mentions.users.first();
   let reason = message.content.split(" ").slice(2).join(" ");
@@ -21,11 +12,9 @@ module.exports.run = (bot, message, args) => {
   if (message.author.id === user.id) return message.reply("você não pode se banir.")
   message.guild.member(user).ban();
   message.reply("usuário **banido** com **sucesso**.").then(a=>a.delete(1500));
-  let modlog = bot.channels.find("name", "punições");
+  let modlog = bot.channels.find("name", "punições"); //troque o nome desse canal para o que desejar ;D
   if (!modlog) return message.reply("Crie um canal chamado de `punições`, não altere nada do nome.");
-  let role = message.guild.roles.find("name", "Dream")
   var embed = new Discord.RichEmbed()
-        .setColor(role.color)
         .setDescription(`**Autor:** ${message.author.username}
 **Membro punido:** ${user.username}
 **Motivo:** ${reason}
